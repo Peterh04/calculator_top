632486660d4b95f464abebf8f65c04bb574b87d4
Peter Heri <heripeter725@gmail.com>
2024-09-19 16:27:28 +0300

    Added functionality to the clear button
     Users can now clear all the buttons displayed on screen.

---
+++ calc.js
@@ -0,0 +1,61 @@
+const buttons = document.querySelectorAll('button');
+const typeScreen = document.querySelector('#typingScreen');
+const clearBtn = document.querySelector('#clearBtn');
+const eraseBtn = document.querySelector('#eraseBtn');
+const equalBtn = document.querySelector('#equalBtn');
+
+// function add(a, b){
+//     return a + b
+// }
+
+
+function appendToInputField(newValue){
+    typeScreen.value += newValue;
+}
+
+function clearInputField(){
+    typeScreen.value = ''
+}
+
+
+
+
+buttons.forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        btn.classList.add('animate')
+        
+        setTimeout(()=>{
+            btn.classList.remove('animate')
+        }, 100)
+    })
+});
+
+// equalBtn.addEventListener('click', ()=>{
+//     console.log('clicked equal')
+//     if(typeScreen.value.includes('+')){
+//         // console.log((Number((typeScreen.value.split('+')).join(''))) + 10)
+//          let a = (Number((typeScreen.value.split('+')).join('')))
+//          let b = 10
+//         console.log(add(a, b))
+//     }
+// })
+
+
+buttons.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        let value = e.currentTarget.value
+        console.log(e.currentTarget.value)
+        appendToInputField(value);
+    })
+    
+})
+
+clearBtn.addEventListener('click', ()=>{
+    clearInputField();
+    console.log('cleared')
+})
+
+
+
+
+
